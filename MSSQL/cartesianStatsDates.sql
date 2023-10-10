0fbd5e1d309924c98f0490121dc2c926e60f7fbe
Tyler Shepherd <100872070+TylerShep@users.noreply.github.com>
2023-10-10 10:52:34 -0600

    Update cartesianStatsDates.sql

--- MSSQL/cartesianStatsDates.sql
+++ MSSQL/cartesianStatsDates.sql
@@ -5,3 +5,18 @@
 -- * Table names, column names, etc. has been aliased to protect DB Schema
 -- =============================================
 
+DECLARE @reportDate datetime, @communityId INT;
+SET @fromDate = DATEADD(day, -730, GETDATE());
+
+WITH DateRange AS (SELECT CAST(GETDATE() AS DATE) AS StatsDate
+                   UNION ALL
+                   SELECT DATEADD(day, -1, StatsDate)
+                   FROM DateRange
+                   WHERE StatsDate > @fromDate)
+
+SELECT d.StatsDate,
+       t1.ID as "TableID"
+FROM DateRange d
+    CROSS JOIN Table1 t1 WITH (NOLOCK)
+ORDER BY 1 DESC, 2 ASC
+OPTION (MAXRECURSION 0);
