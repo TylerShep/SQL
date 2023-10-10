6b254a12548def909e68a4e7cb4cb27368c6f67d
Tyler Shepherd <100872070+TylerShep@users.noreply.github.com>
2023-10-10 09:04:13 -0600

    Create JensenCoWarehouse.sql

---
+++ SnowSQL/JensenCoWarehouse.sql
@@ -0,0 +1,12 @@
+-- =============================================
+-- Author:      Tyler Shepherd
+-- Create date: 2023-10-10
+-- Description: Optizimized legacy stored procedure used for kinesis stream; Reduced run/load time from 1m 50s -> 4s
+-- * Table names, column names, etc. has been aliased to protect DB Schema
+-- =============================================
+
+
+-- Old Legacy Query:
+
+
+-- New Optimized Query:
