c4b6baba71f3ac68402c778a8f3f2b87d928941d
Tyler Shepherd <100872070+TylerShep@users.noreply.github.com>
2024-02-21 13:49:36 -0700

    Update cartesianStatsDates.sql

--- MSSQL/cartesianStatsDates.sql
+++ MSSQL/cartesianStatsDates.sql
@@ -1,8 +1,8 @@
 -- =============================================
--- Author:      Tyler Shepherd
--- Create date: 2023-10-10
--- Description: Creating a temp table that utilizes a cartesian join on IDs to system date, creating a Date for every ID possible
--- * Table names, column names, etc. has been aliased to protect DB Schema
+-- AUTHOR:      Tyler Shepherd
+-- CREATED: 2023-10-10
+-- DESCRIPTION: Creating a temp table that utilizes a cartesian join on IDs to system date, creating a Date for every ID possible
+-- Table names, column names, etc. has been aliased to protect DB Schema
 -- =============================================
 
 DECLARE @reportDate datetime, @communityId INT;
